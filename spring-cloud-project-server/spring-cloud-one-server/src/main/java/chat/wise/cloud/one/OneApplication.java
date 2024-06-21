34b227032d4733cce50bfd627bdf429239164922
siberia.hu <WiseChatTeahouse@gmail.com>
2024-06-21 15:04:48 +0800

    feat:spring-cloud微服务项目基座搭建

---
+++ spring-cloud-project-server/spring-cloud-one-server/src/main/java/chat/wise/cloud/one/OneApplication.java
@@ -0,0 +1,16 @@
+package chat.wise.cloud.one;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author siberia.hu
+ * @Package chat.wise.cloud.one
+ * @Date 2024/6/21 10:38
+ */
+@SpringBootApplication
+public class OneApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OneApplication.class, args);
+    }
+}
